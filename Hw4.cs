ca9a5134bc99ac7f30ed063fb7c49e52ab8c5658
Keigen Godlaski <160975086+godlaska@users.noreply.github.com>
2024-10-27 12:35:15 -0400

    Implemented ZipCodeData class

--- Hw4.cs
+++ Hw4.cs
@@ -1,7 +1,7 @@
 /* 
   Homework#4
 
-  Add your name here: ----
+  Add your name here: Keigen Godlaski
 
   You are free to create as many classes within the Hw4.cs file or across 
   multiple files as you need. However, ensure that the Hw4.cs file is the 
@@ -15,6 +15,8 @@
   
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 public class Hw4
 {
@@ -32,8 +34,10 @@ public class Hw4
 
 
 
-        // TODO: your code goes here
-
+        // Create an instance of FileReader and read the file
+        FileReader reader = new FileReader("zipcodes.txt");
+        List<ZipCodeData> zipCodes = reader.ReadFile();
+        
 
 
 
@@ -53,4 +57,127 @@ public class Hw4
         // Display the elapsed time in milliseconds
         Console.WriteLine($"Elapsed Time: {elapsedTime.TotalMilliseconds} ms");
     }
+
+    // Class to store data for each zip code entry
+  public class ZipCodeData
+  {
+      public int RecordNumber 
+      { 
+          get; set; 
+      }
+      
+      public string Zipcode 
+      { 
+          get; set; 
+      }
+      
+      public string ZipCodeType
+      { 
+          get; set;
+      }
+      
+      public string City
+      { 
+          get; set;
+      }
+      
+      public string State
+      {
+          get; set;
+      }
+      
+      public string LocationType
+      {
+          get; set;
+      }
+      
+      public double Lat 
+      { 
+          get; set; 
+      }
+      
+      public double Long 
+      { 
+          get; set; 
+      }
+      
+      public string Xaxis 
+      { 
+          get; set; 
+      }
+      
+      public string Yaxis 
+      { 
+          get; set; 
+      }
+      
+      public string Zaxis 
+      { 
+          get; set; 
+      }
+      
+      public string WorldRegion 
+      { 
+          get; set; 
+      }
+      
+      public string Country 
+      { 
+          get; set; 
+      }
+      
+      public string LocationText 
+      { 
+          get; set; 
+      }
+      
+      public string Location 
+      { 
+          get; set; 
+      }
+      
+      public bool Decommissioned 
+      { 
+          get; set; 
+      }
+      
+      public int TaxReturnsFiled 
+      { 
+          get; set; 
+      }
+      
+      public int EstimatedPopulation 
+      { 
+          get; set; 
+      }
+      
+      public int TotalWages 
+      { 
+          get; set; 
+      }
+      
+      public string Notes 
+      { 
+          get; set; 
+      }
+  }
+
+  public class FileReader 
+  {
+    private string filePath;
+
+    public FileReader(string inputFilePath) 
+    {
+      filePath = inputFilePath;
+    }
+
+    // Puts record number, zipcode, city, etc. in a list of the class type
+    // ZipCodeData to streamline reading the list after parsing each line
+    public List<ZipCodeData> ReadFile()
+    {
+
+    }
+  }
 }
+
+
